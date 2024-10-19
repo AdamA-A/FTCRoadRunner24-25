7201082fd31ec1abd54ae4932f8aa92cd0eb0f28
AdamA-A <adammnashour@gmail.com>
2024-10-19 14:17:23 -0400

    Integrated OpModes from FtcRobotController

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTester.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Arm;
+
+@TeleOp
+public class ArmTester extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Arm arm = Arm.getInstance();
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            if (gamepad1.y) {
+                arm.extendMotor.setPower(0.1);
+            } else if (gamepad1.a) {
+                arm.extendMotor.setPower(-0.1);
+            } else if (gamepad1.x || gamepad1.b) {
+                arm.extendMotor.setPower(0);
+            }
+            // Show the position of the motor on telemetry
+            telemetry.addData("Encoder Position", arm.extendMotor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Arm {
+    // Counts per revolution
+    final double CPR = 28;
+    private static DcMotor rotationalMotor = null;
+    public static DcMotor extendMotor = null;
+    private static Arm instance = null;
+
+    public static Arm getInstance() {
+        return instance = instance == null ? new Arm() : instance;
+    }
+
+    private Arm() {
+        //rotationalMotor = hardwareMap.dcMotor.get("rotationalMotor");
+        extendMotor = hardwareMap.dcMotor.get("extendMotor");
+
+        //rotationalMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //rotationalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        extendMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //extendMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void rotateTo(double angle, boolean toNormalize) {
+
+        // Convert angle to position
+        int position = angleToTicks((toNormalize ? angle % 360 : angle));
+
+        // Set Position
+        rotationalMotor.setTargetPosition(position);
+    }
+
+    private void extendTo(double inches) {
+
+        // Convert angle to position
+        int position = inchesToTicks(inches);
+
+        // Set Position
+        extendMotor.setTargetPosition(position);
+    }
+    private int inchesToTicks(double inches) {
+        double exactTicks = inches * 0; // TODO find raw ticks, then find ratio of ticks to inches
+        return (int) exactTicks;
+    }
+    private int angleToTicks(double angle) {
+        double revolutions = angle / 360;
+        double exactPosition = revolutions * CPR;
+        return (int) exactPosition;
+    }
+}
